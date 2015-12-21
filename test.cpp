24dd581f15d6c26e5f185ca9ce6d0bb7d728d127
Joshua Bibat <B00071464@student.itb.ie>
2015-12-21 12:33:29 +0000

    Added Login/Logoff Function

--- test.cpp
+++ test.cpp
@@ -1,10 +1,53 @@
-nclude <iostream>
+#include <iostream>
 
 using namespace std;
 
+class Login
+{
+public:
+
+    void SendUsername()
+    {
+        char Username[9];
+
+        while(1)
+        {
+
+            cout << "Login as Student or Admin?\n" << endl;
+            cin >> Username;
+
+            if (Username == 'admin')
+            {
+                Admin a;
+                a.Admin_Function;
+            }
+
+            else if (Username == 'student')
+            {
+                Student s;
+                s.doquiz;
+            }
+
+            else
+            {
+                cout << "Invalid User" << endl;
+
+                break;
+            }
+
+        }
+
+    }
+
+
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
-        return 0;
-	}
+    Login l;
+
+    l.SendUsername();
+
+    return 0;
+}
 
